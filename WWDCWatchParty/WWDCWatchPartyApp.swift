51ad476d1080f690b95f5034daa9de3afb4eec52
Rudrank Riyam <rudrankriyam@gmail.com>
2024-04-11 16:36:55 +0530

    Initial Commit

---
+++ WWDCWatchParty/WWDCWatchPartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDCWatchPartyApp.swift
+//  WWDCWatchParty
+//
+//  Created by Rudrank Riyam on 11/04/24.
+//
+
+import SwiftUI
+
+@main
+struct WWDCWatchPartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
