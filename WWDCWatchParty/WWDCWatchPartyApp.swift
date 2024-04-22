4679495342e04ce193578225258dba835bdfc7b3
Rudrank Riyam <rudrankriyam@gmail.com>
2024-04-22 14:42:51 +0530

    Add code for first blog post

--- WWDCWatchParty/WWDCWatchPartyApp.swift
+++ WWDCWatchParty/WWDCWatchPartyApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct WWDCWatchPartyApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      SessionsView(sessions: .sampleSessions)
     }
+  }
 }
